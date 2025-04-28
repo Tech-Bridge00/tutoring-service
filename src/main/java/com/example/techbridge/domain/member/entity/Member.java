63e016b33d915492761a64969ac38ccd4007bf13
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-04-28 19:47:15 +0900

    Feat: Member 엔티티 생성

---
+++ src/main/java/com/example/techbridge/domain/member/entity/Member.java
@@ -0,0 +1,83 @@
+package com.example.techbridge.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(length = 1)
+    private String gender;
+
+    @Column(nullable = false, length = 11)
+    private String contact;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    private String profileImage;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private String location;
+
+    private Long totalRating;
+    private Long totalMatchCount;
+    private Long totalClassCount;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    public enum Role {
+        STUDENT, TEACHER
+    }
+}
