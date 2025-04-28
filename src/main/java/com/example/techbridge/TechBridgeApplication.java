7431726e35c89f2339b65f6a8cf4850045c2a36a
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-04-28 19:06:02 +0900

    Config: JPA Auditing 추가

--- src/main/java/com/example/techbridge/TechBridgeApplication.java
+++ src/main/java/com/example/techbridge/TechBridgeApplication.java
@@ -2,8 +2,10 @@ package com.example.techbridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TechBridgeApplication {
 
 	public static void main(String[] args) {
