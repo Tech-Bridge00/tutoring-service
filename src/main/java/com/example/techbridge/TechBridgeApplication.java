b89fd3069943faf3c088b25b1a7d48b1243464b2
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-04-28 19:04:49 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/example/techbridge/TechBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.techbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techbridge/TechBridgeApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.techbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBridgeApplicationTests {
+
+}
