bfdcbda755fac18d688b62e856ccb5877b88e393
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-04-28 19:48:50 +0900

    Feat: MemberRepository 생성 및 기본 메소드 추가

---
+++ src/main/java/com/example/techbridge/domain/member/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.example.techbridge.domain.member.repository;
+
+import com.example.techbridge.domain.member.entity.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
