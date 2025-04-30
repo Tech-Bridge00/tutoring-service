bb206aa817955b91470da58a882b163b13d2525a
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-01 02:19:40 +0900

    Feat: Tutor 엔티티 추가 (튜터 정보 테이블 분리)

---
+++ src/main/java/com/example/techbridge/domain/member/entity/Tutor.java
@@ -0,0 +1,36 @@
+package com.example.techbridge.domain.member.entity;
+
+import com.example.techbridge.global.common.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Tutor extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String introduction;
+    private String jobTitle;
+    private String portfolioUrl;
+    private Integer totalExperience;
+    private boolean isCurrentlyEmployed;
+}
