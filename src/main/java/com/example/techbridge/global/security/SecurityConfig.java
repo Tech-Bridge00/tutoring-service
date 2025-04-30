b53d94ff4dbfa9acb76ed2a7c07f5119ccb0e430
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-01 01:34:27 +0900

    Config: 로컬 개발 환경 테스트용 Security 설정 추가

--- src/main/java/com/example/techbridge/global/security/SecurityConfig.java
+++ src/main/java/com/example/techbridge/global/security/SecurityConfig.java
@@ -2,11 +2,35 @@ package com.example.techbridge.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("local")
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers
+                .frameOptions(FrameOptionsConfig::sameOrigin)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/members/**", "/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
