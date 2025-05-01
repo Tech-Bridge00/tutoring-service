2e71c05604b9ded3d583400b233b14a9e3abb3a6
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-01 22:56:07 +0900

    Feat: 학생, 튜터 조회 및 페이징 처리

--- src/main/java/com/example/techbridge/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/techbridge/domain/member/repository/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.example.techbridge.domain.member.repository;
 
 import com.example.techbridge.domain.member.entity.Member;
+import com.example.techbridge.domain.member.entity.Member.Role;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -13,4 +17,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = {"student", "tutor"})
+    Page<Member> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"student", "tutor"})
+    Page<Member> findByRole(Role role, Pageable pageable);
 }
