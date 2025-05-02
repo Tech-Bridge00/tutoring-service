25a4bbe82f50b531173c20ee5c58438ad41e15ee
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-03 04:07:12 +0900

    Feat: 회원가입시 반환되는 응답 상세화 및 회원정보 수정 API 추가

--- src/main/java/com/example/techbridge/domain/member/controller/MemberCommandController.java
+++ src/main/java/com/example/techbridge/domain/member/controller/MemberCommandController.java
@@ -1,6 +1,7 @@
 package com.example.techbridge.domain.member.controller;
 
-import com.example.techbridge.domain.member.dto.MemberResponse;
+import com.example.techbridge.domain.member.dto.MemberDetailResponse;
+import com.example.techbridge.domain.member.dto.MemberUpdateWrapper;
 import com.example.techbridge.domain.member.dto.PasswordChangeRequest;
 import com.example.techbridge.domain.member.dto.SignUpRequestWrapper;
 import com.example.techbridge.domain.member.entity.Member;
@@ -9,6 +10,7 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,11 +26,11 @@ public class MemberCommandController {
     private final MemberCommandService memberCommandService;
 
     @PostMapping
-    public ResponseEntity<MemberResponse> signUp(@Valid @RequestBody SignUpRequestWrapper request) {
+    public ResponseEntity<MemberDetailResponse> signUp(@Valid @RequestBody SignUpRequestWrapper request) {
         Member savedMember = memberCommandService.signUp(request);
         URI location = URI.create("/api/members/" + savedMember.getId());
         return ResponseEntity.created(location)
-            .body(new MemberResponse(savedMember));
+            .body(MemberDetailResponse.of(savedMember));
     }
 
     @PatchMapping("/{id}/password")
@@ -37,5 +39,13 @@ public class MemberCommandController {
 
         memberCommandService.changePassword(id, request);
         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public MemberDetailResponse updateMemberInfo(@PathVariable Long id,
+        @Valid @RequestBody MemberUpdateWrapper request,
+        @AuthenticationPrincipal Member loginMember) {
+        Member updatedMember = memberCommandService.updateMember(id, request, loginMember.getId());
+        return MemberDetailResponse.of(updatedMember);
     }
 }
