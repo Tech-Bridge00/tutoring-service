caa8de8fbc1edb4dc511228b77c683a2938192b7
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-02 16:53:35 +0900

    Feat: SecurityConfig 인증/인가 예외 추가 및 API path 필터 설정

--- src/main/java/com/example/techbridge/global/security/SecurityConfig.java
+++ src/main/java/com/example/techbridge/global/security/SecurityConfig.java
@@ -1,18 +1,52 @@
 package com.example.techbridge.global.security;
 
+import com.example.techbridge.auth.handler.CustomAccessDeniedHandler;
+import com.example.techbridge.auth.handler.CustomAuthenticationEntryPoint;
+import com.example.techbridge.auth.jwt.JwtAuthenticationFilter;
+import com.example.techbridge.auth.jwt.JwtTokenProvider;
+import com.example.techbridge.domain.member.repository.MemberRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Profile({"local", "test"})
 @Configuration
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
+
+    @Autowired
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider,
+        MemberRepository memberRepository,
+        CustomAccessDeniedHandler accessDeniedHandler,
+        CustomAuthenticationEntryPoint authenticationEntryPoint) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberRepository = memberRepository;
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtFilter() {
+        List<String> whiteList = List.of(
+            "/auth/login", "/auth/refresh"
+        );
+        return new JwtAuthenticationFilter(jwtTokenProvider, memberRepository, whiteList);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,12 +57,16 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/h2-console/**").permitAll()
-                .requestMatchers("/api/members/**", "/auth/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/members").permitAll()
+                .requestMatchers("/auth/login", "/auth/refresh").permitAll()
                 .anyRequest().authenticated()
             )
+            .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler))
             .sessionManagement(
                 session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
         return http.build();
     }
 
