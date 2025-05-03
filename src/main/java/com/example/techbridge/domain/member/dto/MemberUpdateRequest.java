9fb5363e3eb91621eaab0dc7b77387b25c810745
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-03 17:47:49 +0900

    Feat: MemberUpdateRequest에 @Builder 추가

--- src/main/java/com/example/techbridge/domain/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/example/techbridge/domain/member/dto/MemberUpdateRequest.java
@@ -7,9 +7,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class MemberUpdateRequest {
 
     @NotBlank(message = "닉네임은 필수입니다.")
