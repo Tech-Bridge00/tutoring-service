4ba8a0a91bc666fe9ed01a4c857edf4f5bf4ff3a
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-05 20:48:09 +0900

    Feat: 과외 목록 조회를 위한 페이징 쿼리와 fetch join 쿼리 추가

--- src/main/java/com/example/techbridge/domain/tutoring/repository/TutoringRepository.java
+++ src/main/java/com/example/techbridge/domain/tutoring/repository/TutoringRepository.java
@@ -1,9 +1,11 @@
 package com.example.techbridge.domain.tutoring.repository;
 
-import com.example.techbridge.domain.tutoring.entity.Tutoring;
 import com.example.techbridge.domain.member.entity.Member;
+import com.example.techbridge.domain.tutoring.entity.Tutoring;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -39,4 +41,58 @@ public interface TutoringRepository extends JpaRepository<Tutoring, Long> {
         @Param("endTime") LocalDateTime endTime,
         @Param("statuses") List<Tutoring.RequestStatus> statuses
     );
+
+    // 로그인한 사용자가 과외 신청한 목록을 ID로 조회 및 시간순으로 정렬
+    @Query("""
+        SELECT t.id FROM Tutoring t
+        WHERE t.requester.id = :requesterId
+        ORDER BY t.startTime DESC
+        """)
+    Page<Long> findTutoringPagesByRequesterId(@Param("requesterId") Long requesterId,
+        Pageable pageable);
+
+    // 로그인한 사용자가 과외 신청을 받은 목록을 ID로 조회 및 시간순으로 정렬
+    @Query("""
+        SELECT t.id FROM Tutoring t
+        WHERE t.receiver.id = :receiverId
+        ORDER BY t.startTime DESC
+        """)
+    Page<Long> findTutoringPagesByReceiverId(@Param("receiverId") Long receiverId,
+        Pageable pageable);
+
+    // Tutor -> Student 과외 신청 목록
+    @Query("""
+        SELECT t FROM Tutoring t
+        JOIN FETCH t.receiver r
+        JOIN FETCH r.student
+        WHERE t.id IN :ids
+        """)
+    List<Tutoring> findWithReceiverStudentByIds(@Param("ids") List<Long> ids);
+
+    // Tutor -> Student 과외 신청 받은 목록
+    @Query("""
+        SELECT t FROM Tutoring t
+        JOIN FETCH t.requester r
+        JOIN FETCH r.student
+        WHERE t.id IN :ids
+        """)
+    List<Tutoring> findWithRequesterStudentByIds(@Param("ids") List<Long> ids);
+
+    // Student -> Tutor 과외 신청 목록
+    @Query("""
+        SELECT t FROM Tutoring t
+        JOIN FETCH t.receiver r
+        JOIN FETCH r.tutor
+        WHERE t.id IN :ids
+        """)
+    List<Tutoring> findWithReceiverTutorByIds(@Param("ids") List<Long> ids);
+
+    // student -> tutor 과외 신청 받은 목록
+    @Query("""
+        SELECT t FROM Tutoring t
+        JOIN FETCH t.requester r
+        JOIN FETCH r.tutor
+        WHERE t.id IN :ids
+        """)
+    List<Tutoring> findWithRequesterTutorByIds(@Param("ids") List<Long> ids);
 }
