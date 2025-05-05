1a0d189792159041772d2fedc16223f050172cbe
DESKTOP-FCF2R90\Geun <jg6735@naver.com>
2025-05-05 22:43:59 +0900

    Refactor: 과외 목록 조회 로직 신청한 목록/신청 받은 목록 조회로 분리

--- src/main/java/com/example/techbridge/domain/tutoring/service/TutoringQueryService.java
+++ src/main/java/com/example/techbridge/domain/tutoring/service/TutoringQueryService.java
@@ -1,18 +1,16 @@
 package com.example.techbridge.domain.tutoring.service;
 
-import com.example.techbridge.domain.member.entity.Member;
 import com.example.techbridge.domain.member.entity.Member.Role;
 import com.example.techbridge.domain.member.exception.MemberNotFoundException;
 import com.example.techbridge.domain.member.repository.MemberRepository;
 import com.example.techbridge.domain.tutoring.dto.ReceiveTutoringSimpleResponse;
 import com.example.techbridge.domain.tutoring.dto.RequestTutoringSimpleResponse;
-import com.example.techbridge.domain.tutoring.dto.TutoringListType;
 import com.example.techbridge.domain.tutoring.dto.TutoringSimpleResponse;
 import com.example.techbridge.domain.tutoring.entity.Tutoring;
 import com.example.techbridge.domain.tutoring.entity.Tutoring.RequestStatus;
-import com.example.techbridge.domain.tutoring.exception.InvalidTutoringRequestTypeException;
 import com.example.techbridge.domain.tutoring.repository.TutoringRepository;
 import jakarta.annotation.Nullable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -32,72 +30,59 @@ public class TutoringQueryService {
     private final TutoringRepository tutoringRepository;
     private final MemberRepository memberRepository;
 
-    public Page<? extends TutoringSimpleResponse> getTutoringList(
-        Long loginMemberId,
-        TutoringListType type,
-        @Nullable RequestStatus statusFilter,
-        Pageable pageable
-    ) {
-        // 로그인한 회원 Role 조회
-        Member member = memberRepository.findById(loginMemberId)
-            .orElseThrow(MemberNotFoundException::new);
-        Role memberRole = member.getRole();
+    public Page<RequestTutoringSimpleResponse> getSentTutoringList(
+        Long loginId, @Nullable RequestStatus status, Pageable pageable) {
+        Role role = getMemberRole(loginId);
 
-        switch (type) {
-            case SENT -> {
-                // 신청한 과외 목록 ID 페이징
-                Page<Long> idPage = tutoringRepository.findTutoringPagesByRequesterIdAndStatus(
-                    loginMemberId, statusFilter, pageable);
-                List<Long> idList = idPage.getContent();
+        return toPage(
+            tutoringRepository.findPageIdListByRequester(loginId, status, pageable),
+            (role == Role.TUTOR)
+                ? tutoringRepository::fetchReceiverStudent
+                : tutoringRepository::fetchReceiverTutor,
+            RequestTutoringSimpleResponse::from,
+            pageable);
+    }
 
-                // 로그인한 회원 role에 따라 수신자 정보 조회
-                List<Tutoring> tutoringList = (memberRole == Role.TUTOR)
-                    ? tutoringRepository.findWithReceiverStudentByIds(idList)
-                    : tutoringRepository.findWithReceiverTutorByIds(idList);
+    public Page<ReceiveTutoringSimpleResponse> getReceivedTutoringList(
+        Long loginId, @Nullable RequestStatus status, Pageable pageable) {
+        Role role = getMemberRole(loginId);
 
-                // dto 변환 후 페이지로 반환
-                return toPage(idList, tutoringList, RequestTutoringSimpleResponse::from, pageable,
-                    idPage.getTotalElements());
-            }
-            case RECEIVED -> {
-                // 신청 받은 과외 목록 ID 페이징
-                Page<Long> idPage = tutoringRepository.findTutoringPagesByReceiverIdAndStatus(
-                    loginMemberId, statusFilter, pageable);
-                List<Long> idList = idPage.getContent();
+        return toPage(
+            tutoringRepository.findPageIdListByReceiver(loginId, status, pageable),
+            (role == Role.TUTOR)
+                ? tutoringRepository::fetchRequesterStudent
+                : tutoringRepository::fetchRequesterTutor,
+            ReceiveTutoringSimpleResponse::from,
+            pageable);
+    }
 
-                // 로그인한 회원 role에 따라 수신자 정보 조회
-                List<Tutoring> tutoringList = (memberRole == Role.TUTOR)
-                    ? tutoringRepository.findWithRequesterStudentByIds(idList)
-                    : tutoringRepository.findWithRequesterTutorByIds(idList);
-
-                // dto 변환 후 페이지로 반환
-                return toPage(idList, tutoringList, ReceiveTutoringSimpleResponse::from, pageable,
-                    idPage.getTotalElements());
-            }
-
-            // sent, received 타입 검증 에러 반환
-            default -> throw new InvalidTutoringRequestTypeException();
-        }
+    // 역할 조회
+    private Role getMemberRole(Long id) {
+        return memberRepository.findById(id)
+            .orElseThrow(MemberNotFoundException::new)
+            .getRole();
     }
 
     // Tutoring 리스트 정렬 및 Page 객체로 변환
     private <T extends TutoringSimpleResponse> Page<T> toPage(
-        List<Long> idList,
-        List<Tutoring> tutoringList,
+        Page<Long> idPage,
+        Function<Collection<Long>, List<Tutoring>> fetchFunction,
         Function<Tutoring, T> mapper,
-        Pageable pageable,
-        long total
-    ) {
-        // id -> tutoring 매핑
-        Map<Long, Tutoring> tutoringMap = tutoringList.stream()
+        Pageable pageable) {
+
+        List<Long> idList = idPage.getContent();
+        if (idList.isEmpty()) {
+            return Page.empty(pageable);
+        }
+
+        Map<Long, Tutoring> map = fetchFunction.apply(idList).stream()
             .collect(Collectors.toMap(Tutoring::getId, Function.identity()));
 
-        // id 기준으로 dto 매핑
         List<T> content = idList.stream()
-            .map(tutoringMap::get)
+            .map(map::get)
             .map(mapper)
             .toList();
 
-        return new PageImpl<>(content, pageable, total);
+        return new PageImpl<>(content, pageable, idPage.getTotalElements());
     }
 }
