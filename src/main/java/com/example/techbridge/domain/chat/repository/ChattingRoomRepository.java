71c69d3e30d045167832e0a3f1c4aee0df4c809f
SHEEN <sheennho@gmail.com>
2025-05-06 15:28:19 +0900

    refacter : Type Changed

--- src/main/java/com/example/techbridge/domain/chat/repository/ChattingRoomRepository.java
+++ src/main/java/com/example/techbridge/domain/chat/repository/ChattingRoomRepository.java
@@ -4,5 +4,5 @@ import com.example.techbridge.domain.chat.model.ChattingRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChattingRoomRepository extends MongoRepository<ChattingRoom, String> {
-    ChattingRoom findByRoomId(String roomId);
+    ChattingRoom findByRoomId(Long roomId);
 }
